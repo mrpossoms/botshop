7fe38b786ee1171cc44fb5fc6966ac5dd35e1d6c
Kirk Roerig <mr.possoms@gmail.com>
2016-11-16 00:10:12 +0000

    Improved the findlibs script. Added logic for failure to detect lib

--- findlibs.sh
+++ findlibs.sh
@@ -5,7 +5,13 @@ cat /dev/null > findings.mk
 function find_lib_dir {
 	LIB=$1
 	LIB_PATH=$(find /opt /usr/local -name $LIB -print -quit)
-	echo ${LIB_PATH/\/$LIB/}
+
+	if [ -z $LIB_PATH ]
+	then
+		echo "";
+	else
+		echo ${LIB_PATH/\/$LIB/};
+	fi
 }
 
 MAKE_LIB=
@@ -18,6 +24,14 @@ for DEPENDENCY in $(cat depends); do
 	echo $DEPENDENCY;
 	LIB_DIR=$(find_lib_dir $DEPENDENCY)
 	INC_DIR=${LIB_DIR/lib/include}
+
+
+	if [ -z $LIB_DIR ]
+	then
+		printf "Couldn't find: %s\n" $DEPENDENCY;
+		exit -1;
+	fi
+
 	MAKE_LIB+=$(printf " -L%s" $LIB_DIR);
 	MAKE_INC+=$(printf " -I%s" $INC_DIR)
 done
