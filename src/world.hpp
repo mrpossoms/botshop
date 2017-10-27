528264570e9047fe1e01f34c60c53b08bd244afa
Kirk Roerig <kirk@kirkroerig.com>
2017-10-27 14:03:31 -0400

    Working on PBR shader and world object

---
+++ src/world.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "core.h"
+
+namespace botshop
+{
+
+class Field
+{
+	virtual int id() = 0;
+	virtual Vec3 vectorAt(Vec3& point, float t) = 0;
+};
+
+class World : Updateable, Drawable
+{
+private:
+	dWorldID ode_world;
+	vector<Field> feilds;
+	vector<Field> forces;
+
+public:
+	World();
+	~World();
+
+	void step(float dt);
+};
+
+}
