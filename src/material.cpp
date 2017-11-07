066ca91b0c1ae0e00640cff271c5f97854bd475f
Kirk Roerig <mr.possoms@gmail.com>
2017-11-06 23:57:28 -0500

    Pulled texture creation into it's own function

--- src/material.cpp
+++ src/material.cpp
@@ -7,6 +7,37 @@
 using namespace botshop;
 
 
+GLuint create_texture(int width, int height, GLenum format, void* pixel_buf)
+{
+	GLuint tex;
+
+	assert(gl_get_error());
+	glGenTextures(1, &tex);
+	assert(gl_get_error());
+
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 10);
+	glTexImage2D(
+		GL_TEXTURE_2D,
+		0,
+		format,
+		width, height,
+		0,
+		format,
+		GL_UNSIGNED_BYTE,
+		(void*)pixel_buf
+	);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	return tex;
+}
+
+
 void abort(std::string message)
 {
 	std::cerr << message << std::endl;
@@ -129,28 +160,7 @@ GLuint MaterialFactory::load_texture(std::string path)
 
 	fclose(fp);
 
-	assert(gl_get_error());
-	glGenTextures(1, &tex);
-	assert(gl_get_error());
-
-	glBindTexture(GL_TEXTURE_2D, tex);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 10);
-	glTexImage2D(
-		GL_TEXTURE_2D,
-		0,
-		gl_color_type,
-		width, height,
-		0,
-		gl_color_type,
-		GL_UNSIGNED_BYTE,
-		(void*)pixel_buf
-	);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glGenerateMipmap(GL_TEXTURE_2D);
+	tex = create_texture(width, height, gl_color_type, pixel_buf);
 
 	assert(gl_get_error());
 
